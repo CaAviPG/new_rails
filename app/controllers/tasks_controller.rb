a50e4a49e1085eabce79ba8e5ca5374a2d04c5fc
Caren Pampas <carenpg15@gmail.com>
2022-01-27 08:51:07 -0500

    finished

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,60 @@
 class TasksController < ApplicationController
-  def task
+  def index
+    @tasks = Task.all
   end
+
+  def new
+    @task = Task.new # needed to instantiate the form_for
+  end
+
+  def show
+    @task = Task.find(params[:id])
+    # if @task == true
+    #   return "the task is completed!"
+    # else
+    #   return "the task is not completed yet"
+    # end
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    # no need for app/views/tasks/create.html.erb
+    redirect_to task_path(@task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+
+    # no need for app/views/tasks/update.html.erb
+    redirect_to task_path(@task)
+    # Will raise ActiveModel::ForbiddenAttributesError
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    # no need for app/views/tasks/destroy.html.erb
+    redirect_to tasks_path
+  end
+  #  def create
+  #   @task = Task.new(task_params)
+  #   @task.save
+
+  #   # no need for app/views/tasks/create.html.erb
+  #   redirect_to task_path(@task)
+  # end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
 end
